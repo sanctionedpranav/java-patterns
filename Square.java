31545682b1875ce58ef072e7ec65e4c910939d95
sanctionedpranav <pranav9728@gmail.com>
2025-10-29 12:08:46 +0530

    square pattern

---
+++ Square.java
@@ -0,0 +1,13 @@
+public class Square {
+  public static void main(String[] args) {
+    int num = 5;
+
+    for (int row = 1; row <= num; row++) {
+      for (int col = 1; col <= num; col++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+
+    }
+  }
+}
