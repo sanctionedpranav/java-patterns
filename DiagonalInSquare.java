f6964e6de91d2b24bf660a4bf2c236e0fba4c991
sanctionedpranav <pranav9728@gmail.com>
2025-11-17 15:09:44 +0530

    diagonal in square star pattern done... space changes in hollow square and solid square

--- DiagonalInSquare.java
+++ DiagonalInSquare.java
@@ -1,16 +1,17 @@
-public class HollowSquare {
+public class DiagonalInSquare {
   public static void main(String[] args) {
     int num = 5;
 
     for (int row = 1; row <= num; row++) {
       for (int col = 1; col <= num; col++) {
-        if (row == 1 || row == num || col == 1 || col == num) {
+        if (row == 1 || row == num || col == 1 || col == num || row == col || col == num - row + 1) {
           System.out.print("* ");
         } else {
           System.out.print("  ");
         }
       }
-      System.out.println(" ");
+      System.out.println("");
+
     }
   }
 }
--- Square.java
+++ Square.java
@@ -4,7 +4,7 @@ public class Square {
 
     for (int row = 1; row <= num; row++) {
       for (int col = 1; col <= num; col++) {
-        System.out.print("*");
+        System.out.print("* ");
       }
       System.out.println("");
 
