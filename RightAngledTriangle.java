7283d7b2c3ca31beb8264724a56e8646ed939286
sanctionedpranav <pranav9728@gmail.com>
2025-11-28 13:43:58 +0530

    right angled triangle using recursion

--- RightAngledTriangle.java
+++ RightAngledTriangle.java
@@ -1,15 +1,35 @@
 public class RightAngledTriangle {
-  public static void main(String[] args) {
-    int num = 5;
-
-    for (int row = 1; row <= num; row++) {
-      for (int col = 1; col <= num; col++) {
-        if(col <= row){
-          System.out.print("*");
-        }
-          System.out.print(" ");
-      }
-          System.out.println("");
+  static void printStar(int star) {
+    if (star == 0) {
+      return;
     }
+    System.out.print("* ");
+    printStar(star - 1);
+  }
+
+  static void printLine(int lines) {
+    if (lines == 0) {
+      return;
+    }
+
+    printLine(lines - 1);
+    printStar(lines);
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    printLine(5);
+
+    // int num = 5;
+
+    // for (int row = 1; row <= num; row++) {
+    // for (int col = 1; col <= num; col++) {
+    // if (col <= row) {
+    // System.out.print("*");
+    // }
+    // System.out.print(" ");
+    // }
+    // System.out.println("");
+    // }
   }
 }
